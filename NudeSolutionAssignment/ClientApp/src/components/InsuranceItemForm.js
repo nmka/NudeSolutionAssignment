ccae0ae72c6ced6d6a0c1a7d1b65ef9ae4877f21
Nyamjav Jambaljav <jambaljav2073@saskpolytech.ca>
2023-04-12 08:41:46 -0600

    Form value input decrement and increment

--- NudeSolutionAssignment/ClientApp/src/components/InsuranceItemForm.js
+++ NudeSolutionAssignment/ClientApp/src/components/InsuranceItemForm.js
@@ -1,5 +1,6 @@
 
 import React, { useId, useState } from 'react';
+import { AiFillCaretUp, AiFillCaretDown } from 'react-icons/ai'
 
 const InsuranceItemForm = ({addItem, categories}) => {
 
@@ -13,11 +14,45 @@ const InsuranceItemForm = ({addItem, categories}) => {
         setValue('');
     }
 
+    const changeValueInput= (e) =>
+        setValue((v) => (e.target.validity.valid ? e.target.value : v))
+
+    const increment= () => {
+        setValue(parseInt(value) + 1);
+    }
+    const decrement = () => {
+        if(value > 0) setValue(parseInt(value) - 1);
+    }
+
     return (<main className ='insurance-form'>
 
-        <input className='name-input' placeholder='name' name="Name" value={name} onChange={e => setName(e.target.value)} />
-        <input className='value-input' placeholder='value'  name="Value" value={value} onChange={e => setValue(e.target.value)} />
-        <select className='category-input' value={categoryId} onChange={e => setCategoryId(e.target.value)}>
+        <input
+            className='name-input'
+            placeholder='name'
+            name="Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+        />
+        <input
+            className='value-input'
+            placeholder='value'
+            name="Value"
+            value={value}
+            pattern="[0-9]*"
+            onChange={e => changeValueInput(e)}
+        />
+        <div className='grid'>
+            <AiFillCaretUp className='clickable' onClick={increment} />
+            <AiFillCaretDown className='clickable' onClick={decrement} />
+        </div>
+        
+        
+
+        <select
+            className='category-input'
+            value={categoryId}
+            onChange={e => setCategoryId(e.target.value)}
+        >
             {categories.map((category) => <option key={category.id} value={category.id} > {category.name}</option >)}
         </select>
         <button className='submit-btn' onClick={submit}>Add</button>
